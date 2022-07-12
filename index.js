a912b6ac29961c54ef13eb447fcb779ba77d1dab
Maximilyanus29 <vladek2000@inbox.ru>
2022-07-12 15:23:27 +0300

    12.07.2022

--- index.js
+++ index.js
@@ -18,3 +18,52 @@ var app3 = new Vue({
         seen: true
     }
 })
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Изучить JavaScript' },
+            { text: 'Изучить Vue' },
+            { text: 'Создать что-нибудь классное' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Привет, Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Привет, Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    // Компонент todo-item теперь принимает
+    // "prop", то есть входной параметр.
+    // Имя входного параметра todo.
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Овощи' },
+            { id: 1, text: 'Сыр' },
+            { id: 2, text: 'Что там ещё люди едят?' }
+        ]
+    }
+})
+
